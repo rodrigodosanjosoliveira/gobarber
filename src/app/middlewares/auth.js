3c27e99cdd9d7703bb919cf5c5e16c486ed56565
Rodrigo Oliveira <rodrigodosanjosoliveira@gmail.com>
2019-11-17 15:56:54 -0300

    validation

--- src/app/middlewares/auth.js
+++ src/app/middlewares/auth.js
@@ -15,12 +15,10 @@ export default async (req, res, next) => {
   try {
     const decoded = await promisify(jwt.verify)(token, authConfig.secret);
 
-    console.log(decoded);
+    req.userId = decoded.id;
 
     return next();
   } catch (err) {
     return res.status(401).json({ error: 'Token invalid' });
   }
-
-  return next();
 };
